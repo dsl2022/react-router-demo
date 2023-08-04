7317b5700fb03535fb5eb09771270a4f34758c29
dsl2022 <2015rpro@gmail.com>
2023-08-03 21:07:22 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<App />} />
+        <Route path="/dennys" element={<App />} />
+        <Route path="/alberto" element={<App />} />
+        <Route path="/chic-fil-a" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
